0aaa7dfd51459bf932577544b381d2c7a01e50e8
ayj71 <ayj7137@naver.com>
2022-06-06 19:27:37 +0900

    [220606]Backjoon Practice

---
+++ Desktop/자바정석_basic-master/javajungsuk_basic_src/backJoon220606/src/main/Main01.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+//8393 ÇÕ ¹®Á¦
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i <= n; i++) {
+			System.out.println(i+n);
+			
+		}
+		
+	}
+
+}
